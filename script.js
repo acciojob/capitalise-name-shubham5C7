b2d584a20495b00ffd6f944b4eca7716e77eca02
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-16 14:36:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 const fN = document.getElementById("fname");
 
-fN.addEventListener("input",()=>{
+fN.addEventListener("mouseout",()=>{
 	fN.value = fN.value.toUpperCase();
 })
